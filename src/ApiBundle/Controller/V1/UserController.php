e2fc7a6af6e020a7e6f8180676d28496df596be0
jahanzaibraza13 <jahanzaibraza13@gmail.com>
2022-02-20 22:17:17 +0500

    display error message in update user api when 500

--- src/ApiBundle/Controller/V1/UserController.php
+++ src/ApiBundle/Controller/V1/UserController.php
@@ -718,7 +718,7 @@ class UserController extends AbstractController
             );
         } catch (\Exception $exception) {
             $userLogger->error('[update_user_api]: ' . $exception->getMessage());
-            return $utilService->makeResponse(Response::HTTP_INTERNAL_SERVER_ERROR, CommonEnum::INTERNAL_SERVER_ERROR_TEXT);
+            return $utilService->makeResponse(Response::HTTP_INTERNAL_SERVER_ERROR, $exception->getMessage());
         }
     }
 
